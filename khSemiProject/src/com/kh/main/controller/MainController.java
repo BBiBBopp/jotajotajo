57ba25ce19ef6f9356d850b090f3fa04789524af
JeongUk Cho <wjddnr8741@naver.com>
2022-02-01 22:21:41 +0900

    Update MainController.java

--- khSemiProject/src/com/kh/main/controller/MainController.java
+++ khSemiProject/src/com/kh/main/controller/MainController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.common.model.vo.PageInfo;
 import com.kh.movie.model.service.MovieService;
 import com.kh.movie.model.vo.Movie;
 import com.kh.movie.model.vo.Picture;
@@ -42,30 +43,29 @@ public class MainController extends HttpServlet {
 		
 		request.setAttribute("mainList", mainList);
 		
+		int currentPage = 1;
+		if(request.getParameter("currentPage") != null)
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		
+		int listCount= new MovieService().countCurrentMovie();
+		int pageLimit = 5; // 페이지 하단에 보여질 페이징바의 최대 갯수
+		int boardLimit = 5; //한페이지에 보여질 최대 갯수
+		
+		PageInfo pi = new PageInfo().calcPageInfo(listCount, currentPage, pageLimit, boardLimit);
 		
 		
-		
-		int sCount = 1;
-		int eCount = 5;
-//		int sCount = Integer.parseInt(request.getParameter("startCount"));
-//		int eCount = Integer.parseInt(request.getParameter("endCount"));
-
-		ArrayList<Movie> currentList = new MovieService().selectCurrentList(sCount, eCount);
-		
+		ArrayList<Movie> currentList = new MovieService().selectCurrentList(pi.getStartRow(), pi.getEndRow());
+		//해당하는 영화 번호 추출
 		ArrayList<Integer> movieNoList = new ArrayList<>();
 		for(Movie mv : currentList)
 			movieNoList.add(mv.getMovieNo());
-		
+		//추출된 번호로 포스터 조회해오기
 		ArrayList<Picture> picList = new MovieService().selectPosterList(movieNoList);
-				
+	
 		
+		request.setAttribute("pi", pi);
 		request.setAttribute("currentList", currentList);
 		request.setAttribute("picList", picList);
-		
-		
-		
-		
-		
 		
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 		
